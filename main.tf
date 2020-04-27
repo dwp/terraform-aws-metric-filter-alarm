8e31d992c71085eeabe64c957b245f75b297737b
Mike Winker <mike.winker@dwp.gov.uk>
2020-04-27 18:47:49 +0100

    Update main.tf to use tf12 formatting

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 resource "aws_cloudwatch_log_metric_filter" "metric_filter" {
-  log_group_name = "${var.log_group_name}"
+  log_group_name = var.log_group_name
   metric_transformation {
-    name = "${var.metric_filter_name != "" ? var.metric_filter_name : var.alarm_name}"
-    namespace = "${var.metric_namespace}"
-    value = "${var.metric_value}"
+    name = var.metric_filter_name != "" ? var.metric_filter_name : var.alarm_name
+    namespace = var.metric_namespace
+    value = var.metric_value
   }
-  name = "${var.metric_filter_name != "" ? var.metric_filter_name : var.alarm_name}"
-  pattern = "${var.pattern}"
+  name = var.metric_filter_name != "" ? var.metric_filter_name : var.alarm_name
+  pattern = var.pattern
 }
 
 resource "aws_cloudwatch_metric_alarm" "metric_alarm" {
-  alarm_name = "${var.alarm_name}"
-  comparison_operator = "${var.comparison_operator}"
-  evaluation_periods = "${var.evaluation_periods}"
-  metric_name = "${aws_cloudwatch_log_metric_filter.metric_filter.name}"
-  namespace = "${var.metric_namespace}"
-  period = "${var.period}"
-  threshold = "${var.threshold}"
-  statistic = "${var.statistic}"
-  alarm_actions = "${var.alarm_action_arns}"
+  alarm_name = var.alarm_name
+  comparison_operator = var.comparison_operator
+  evaluation_periods = var.evaluation_periods
+  metric_name = aws_cloudwatch_log_metric_filter.metric_filter.name
+  namespace = var.metric_namespace
+  period = var.period
+  threshold = var.threshold
+  statistic = var.statistic
+  alarm_actions = var.alarm_action_arns
 }
