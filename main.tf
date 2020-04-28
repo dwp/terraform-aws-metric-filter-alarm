289de4af6fcdb8e01282682b0986abc837277005
mike-iw <61879747+mike-iw@users.noreply.github.com>
2020-04-28 09:40:13 +0100

    Update for tf12 (#5)
    * Update main.tf to use tf12 formatting
    
    * Variables file needed updating for tf12 too
    
    * Formatting
    
    * Formatting
    
    Co-authored-by: Mike Winker <mike.winker@dwp.gov.uk>

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 resource "aws_cloudwatch_log_metric_filter" "metric_filter" {
   log_group_name = var.log_group_name
   metric_transformation {
-    name = var.metric_filter_name != "" ? var.metric_filter_name : var.alarm_name
+    name      = var.metric_filter_name != "" ? var.metric_filter_name : var.alarm_name
     namespace = var.metric_namespace
-    value = var.metric_value
+    value     = var.metric_value
   }
-  name = var.metric_filter_name != "" ? var.metric_filter_name : var.alarm_name
+  name    = var.metric_filter_name != "" ? var.metric_filter_name : var.alarm_name
   pattern = var.pattern
 }
 
 resource "aws_cloudwatch_metric_alarm" "metric_alarm" {
-  alarm_name = var.alarm_name
+  alarm_name          = var.alarm_name
   comparison_operator = var.comparison_operator
-  evaluation_periods = var.evaluation_periods
-  metric_name = aws_cloudwatch_log_metric_filter.metric_filter.name
-  namespace = var.metric_namespace
-  period = var.period
-  threshold = var.threshold
-  statistic = var.statistic
-  alarm_actions = var.alarm_action_arns
+  evaluation_periods  = var.evaluation_periods
+  metric_name         = aws_cloudwatch_log_metric_filter.metric_filter.name
+  namespace           = var.metric_namespace
+  period              = var.period
+  threshold           = var.threshold
+  statistic           = var.statistic
+  alarm_actions       = var.alarm_action_arns
 }
--- variables.tf
+++ variables.tf
@@ -1,67 +1,67 @@
 variable "log_group_name" {
-  type = "string"
+  type        = string
   description = "The name of the log group to associate the metric filter with"
 }
 
 variable "metric_namespace" {
-  type = "string"
+  type        = string
   description = "The destination namespace of the CloudWatch metric"
 }
 
 variable "metric_value" {
-  type = "string"
+  type        = string
   description = "The value of the CloudWatch metric"
-  default = "1"
+  default     = "1"
 }
 
 variable "pattern" {
-  type = "string"
+  type        = string
   description = "A valid CloudWatch Logs filter pattern for extracting metric data out of ingested log events"
 }
 
 variable "alarm_name" {
-  type = "string"
+  type        = string
   description = "The descriptive name for the alarm. This name must be unique within the user's AWS account"
 }
 
 variable "metric_filter_name" {
-  type = "string"
+  type        = string
   description = "A name for the metric filter"
-  default = ""
+  default     = ""
 }
 
 variable "alarm_action_arns" {
-  type = "list"
+  type        = list(string)
   description = "The list of actions to execute when this alarm transitions into an ALARM state from any other state. Each action is specified as an Amazon Resource Number (ARN)"
-  default = []
+  default     = []
 }
 
 variable "comparison_operator" {
-  type = "string"
+  type        = string
   description = "The arithmetic operation to use when comparing the specified Statistic and Threshold. The specified Statistic value is used as the first operand. Either of the following is supported: GreaterThanOrEqualToThreshold, GreaterThanThreshold, LessThanThreshold, LessThanOrEqualToThreshold"
-  default = "GreaterThanThreshold"
+  default     = "GreaterThanThreshold"
 }
 
 variable "evaluation_periods" {
-  type = "string"
+  type        = string
   description = "The number of periods over which data is compared to the specified threshold"
-  default = "1"
+  default     = "1"
 }
 
 variable "period" {
-  type = "string"
+  type        = string
   description = "The period in seconds over which the specified statistic is applied"
-  default = "60"
+  default     = "60"
 }
 
 variable "threshold" {
-  type = "string"
+  type        = string
   description = "The value against which the specified statistic is compared"
-  default = "0"
+  default     = "0"
 }
 
 variable "statistic" {
-  type = "string"
+  type        = string
   description = "The statistic to apply to the alarm's associated metric. Either of the following is supported: SampleCount, Average, Sum, Minimum, Maximum"
-  default = "Sum"
+  default     = "Sum"
 }
