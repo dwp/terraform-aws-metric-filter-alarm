fca86e5d6bea76ef51ba4b71635a093ccb7c5780
David Birks <david@birks.dev>
2021-08-12 11:26:20 -0400

    Add a variable for `treat_missing_data`

--- main.tf
+++ main.tf
@@ -20,6 +20,7 @@ resource "aws_cloudwatch_metric_alarm" "metric_alarm" {
   threshold           = var.threshold
   statistic           = var.statistic
   alarm_actions       = var.alarm_action_arns
+  treat_missing_data  = var.treat_missing_data
   tags                = { 
       notification_type = var.notification_type,
       severity = var.severity,
